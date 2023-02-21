d8b5fc2882fd3530b3cb3fe2fafb0f81c1e21f49
Dominik Kolarsky <kolarskydominik@gmail.com>
2023-02-21 21:41:08 +0100

    add chache clear

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import App from "./App";
-import ReactDOM from "react-dom/client";
+import App from './App';
+import ReactDOM from 'react-dom/client';
+import { ClearCacheProvider, useClearCacheCtx } from 'react-clear-cache';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ClearCacheProvider duration={5000}>
+    <App />
+  </ClearCacheProvider>
+);
